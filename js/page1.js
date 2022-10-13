a792891e86744256767ed5e7f56a281146177809
euna99 <a653bc@naver.com>
2022-10-13 23:10:32 +0900

    1013 23

---
+++ js/page1.js
@@ -0,0 +1,17 @@
+
+
+// let hamberger = document.querySelector("#hamberger");
+// let test=document.querySelector("#test");
+// hamberger.addEventListener("mouseover",function(event){
+//     test.style.display="block";
+// })
+// hamberger.addEventListener("mouseout",function(event){
+//     test.style.display="none";
+// })
+
+//hamberger 버튼 
+let sign=document.querySelector("#sign")
+let sign_div=document.querySelector(".sign_div")
+sign.addEventListener("click",function(event){
+    sign_div.style.display="block"
+})
