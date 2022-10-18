4e71fe85f64af8a2e9c084871446d7e80e58bf4b
euna99 <a653bc@naver.com>
2022-10-18 17:53:55 +0900

    sign// hover 대략적인 틀

--- js/page1.js
+++ js/page1.js
@@ -1,17 +1,9 @@
-
-
-// let hamberger = document.querySelector("#hamberger");
-// let test=document.querySelector("#test");
-// hamberger.addEventListener("mouseover",function(event){
-//     test.style.display="block";
-// })
-// hamberger.addEventListener("mouseout",function(event){
-//     test.style.display="none";
-// })
-
-//hamberger 버튼 
 let sign=document.querySelector("#sign")
-let sign_div=document.querySelector(".sign_div")
+let sign_close_btn=document.querySelector(".sign_close_btn")
+let sign_background=document.querySelector(".sign_background")
 sign.addEventListener("click",function(event){
-    sign_div.style.display="block"
+    sign_background.style.display="block"
+})
+sign_close_btn.addEventListener("click",function(event){
+    sign_background.style.display="none"
 })
